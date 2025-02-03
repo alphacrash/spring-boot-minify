1818da3161357f88f34cd2e75c9c61a8a0d5a3b9
alphacrash <sudhansu.ace@gmail.com>
2025-02-03 15:56:55 +0530

    Updated Encoder

--- src/main/java/com/alphacrash/minify/util/MinifyUtil.java
+++ src/main/java/com/alphacrash/minify/util/MinifyUtil.java
@@ -1,13 +1,33 @@
 package com.alphacrash.minify.util;
 
-import java.util.Base64;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class MinifyUtil {
-    public static String encode(String input) {
-        String encoded = Base64.getUrlEncoder().encodeToString(input.getBytes());
-        if (encoded.length() > 7) {
-            return encoded.substring(0, 7);
+    private static final String BASE62 = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    public static String encode(String url) {
+        try {
+            // Create SHA-256 hash of the URL
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hashBytes = digest.digest(url.getBytes());
+
+            // Convert the byte array into a BigInteger
+            BigInteger bigInt = new BigInteger(1, hashBytes);
+
+            // Encode the BigInteger in Base62
+            StringBuilder shortenedUrl = new StringBuilder();
+            while (bigInt.compareTo(BigInteger.ZERO) > 0) {
+                int remainder = bigInt.mod(BigInteger.valueOf(62)).intValue();
+                shortenedUrl.append(BASE62.charAt(remainder));
+                bigInt = bigInt.divide(BigInteger.valueOf(62));
+            }
+
+            // Return the first 7 characters from the Base62 encoded string
+            return shortenedUrl.reverse().substring(0, 7);
+        } catch (NoSuchAlgorithmException e) {
+            return null;
         }
-        return encoded;
     }
 }
